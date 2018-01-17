e86d383eab09e139e7d45ae5049493fbe52b01ce
LaurentNevou <35040499+LaurentNevou@users.noreply.github.com>
2018-01-17 15:06:40 +0100

    Add files via upload

--- Pot_GaN_AlN.m
+++ Pot_GaN_AlN.m
@@ -86,7 +86,7 @@ V0=V0-min(min(min(V0)));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-figure('Name','Potential','position',[-1900 100 1600 800])
+figure('Name','Potential','position',[100 100 1600 800])
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -94,12 +94,18 @@ subplot(2,3,1,'fontsize',15)
 hold on;grid on;
 
 pcolor(x*1e9,z*1e9,squeeze(V0(round(end/2),:,:))')
-plot([-1 1]*10,[0 0],'w','linewidth',2)
-plot([1 1]*5,[-1 1]*10,'w','linewidth',2)
-plot([1 1]*-5,[-1 1]*10,'w','linewidth',2)
+plot([-1 1]*10,[0 0],'w--','linewidth',2)
 
-colormap(cool)
+idz=find(z>QDh);
+idz=idz(1)-1;
+plot([-1 1]*10,[1 1]*z(idz)*1e9,'w--','linewidth',2)
+plot([1 1]*0,[-1 1]*10,'b--','linewidth',2)
+plot([1 1]*x(end)*1e9,[-1 1]*10,'r--','linewidth',2)
+
+colormap(jet)
 colorbar
+shading flat
+caxis([floor(min(V0(:)))  ceil(max(V0(:)))])
 
 xlim([-1 1]*Mx/2*1e9)
 ylim([-1 1]*My/2*1e9)
@@ -114,12 +120,17 @@ subplot(2,3,4,'fontsize',15)
 hold on;grid on;
 
 pcolor(y*1e9,z*1e9,squeeze(V0(:,round(end/2),:))')
-plot([-1 1]*10,[0 0],'w','linewidth',2)
-plot([1 1]*5,[-1 1]*10,'w','linewidth',2)
-plot([1 1]*-5,[-1 1]*10,'w','linewidth',2)
+plot([-1 1]*10,[0 0],'w--','linewidth',2)
+idz=find(z>QDh);
+idz=idz(1)-1;
+plot([-1 1]*10,[1 1]*z(idz)*1e9,'w--','linewidth',2)
+plot([1 1]*0,[-1 1]*10,'b--','linewidth',2)
+plot([1 1]*x(end)*1e9,[-1 1]*10,'r--','linewidth',2)
 
-colormap(cool)
+colormap(jet)
 colorbar
+shading flat
+caxis([floor(min(V0(:)))  ceil(max(V0(:)))])
 
 xlim([-1 1]*Mx/2*1e9)
 ylim([-1 1]*My/2*1e9)
@@ -138,14 +149,10 @@ idz=idz(1);
 
 pcolor(x*1e9,y*1e9,squeeze(V0(:,:,idz)))
 
-plot([-1 1]*10,[1 1]*5,'w','linewidth',2)
-plot([-1 1]*10,[1 1]*-5,'w','linewidth',2)
-
-plot([1 1]*5,[-1 1]*10,'w','linewidth',2)
-plot([1 1]*-5,[-1 1]*10,'w','linewidth',2)
-
-colormap(cool)
+colormap(jet)
 colorbar
+shading flat
+caxis([floor(min(V0(:)))  ceil(max(V0(:)))])
 
 xlim([-1 1]*My/2*1e9)
 ylim([-1 1]*My/2*1e9)
@@ -163,14 +170,10 @@ idz=find(z>QDh);
 idz=idz(1)-1;
 pcolor(x*1e9,y*1e9,squeeze(V0(:,:,idz)))
 
-plot([-1 1]*10,[1 1]*5,'w','linewidth',2)
-plot([-1 1]*10,[1 1]*-5,'w','linewidth',2)
-
-plot([1 1]*5,[-1 1]*10,'w','linewidth',2)
-plot([1 1]*-5,[-1 1]*10,'w','linewidth',2)
-
-colormap(cool)
+colormap(jet)
 colorbar
+shading flat
+caxis([floor(min(V0(:)))  ceil(max(V0(:)))])
 
 xlim([-1 1]*My/2*1e9)
 ylim([-1 1]*My/2*1e9)
@@ -186,7 +189,56 @@ hold on;grid on;
 plot(z*1e9,squeeze(V0(round(end/2),round(end/2),:)) ,'b.-')
 plot(z*1e9,squeeze(V0(round(end/2),end,:)) ,'r.-')
 
+ylim([floor(min(V0(:)))  ceil(max(V0(:)))])
 xlabel('z (nm)')
 ylabel('Energy (eV)')
 title(strcat('Potential (eV): Vz @y=0nm and \color{blue}x=0nm ; \color{red}x=',num2str(x(end)*1e9),'nm'))
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+subplot(2,3,6,'fontsize',15)
+hold on;grid on;
+
+view (-38, 30);
+
+
+aa=[
+-sqrt(3)/2*a  -a/2   0
+-sqrt(3)/2*a  +a/2   0
+0             +a     0
++sqrt(3)/2*a  +a/2   0
++sqrt(3)/2*a  -a/2   0
+0             -a     0
+-sqrt(3)/2*a  -a/2   0
+]*1e9;
+
+plot3(aa(:,1),aa(:,2),aa(:,3),'k','linewidth',2)
+
+bb=[
++(QDh-h)/h*sqrt(3)/2*a  -(h-QDh)*a/h/2   QDh
++(QDh-h)/h*sqrt(3)/2*a  +(h-QDh)*a/h/2   QDh
+0                       +(h-QDh)*a/h     QDh
+
+-(QDh-h)/h*sqrt(3)/2*a  +(h-QDh)*a/h/2   QDh
+-(QDh-h)/h*sqrt(3)/2*a  -(h-QDh)*a/h/2   QDh
+0                       -(h-QDh)*a/h     QDh
++(QDh-h)/h*sqrt(3)/2*a  -(h-QDh)*a/h/2   QDh
+]*1e9;
+
+plot3(bb(:,1),bb(:,2),bb(:,3),'k','linewidth',2)
+
+
+for j=1:length(aa(:,1))
+  
+  plot3([aa(j,1) bb(j,1)],[aa(j,2) bb(j,2)],[aa(j,3) bb(j,3)],'k','linewidth',2)
+
+end
+
+M=max([Mx My]);
+xlim([-1 1]*M/3*1e9)
+ylim([-1 1]*M/3*1e9)
+zlim([-0.8 0.8]*M/3*1e9)
+
+xlabel('x (nm)')
+ylabel('y (nm)')
+zlabel('z (nm)')
